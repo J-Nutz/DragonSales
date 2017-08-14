1ee8daeffd91f67f998acd5ba8a8283b4d73be05
Jonah <jonah.stieglitz17@gmail.com>
2017-08-13 19:26:51 -0500

    Idek What All Happened

---
+++ src/main/java/Employee.java
@@ -0,0 +1,34 @@
+/*
+ * Created by Jonah on 4/11/2017.
+ */
+
+public class Employee
+{
+    private int id;
+    private String fullName;
+
+    public Employee()
+    {
+
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int newId)
+    {
+        this.id = newId;
+    }
+
+    public String getFullName()
+    {
+        return fullName;
+    }
+
+    public void setFullName(String newFullName)
+    {
+        this.fullName = newFullName;
+    }
+}
---
+++ src/main/java/Employee2.java
@@ -0,0 +1,14 @@
+/*
+ * Created by Jonah on 4/11/2017.
+ */
+
+public class Employee2
+{
+    public int id;
+    public String fullName;
+
+    public Employee2()
+    {
+
+    }
+}
